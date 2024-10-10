46a0bbcda0e8e3ee4cbc2726e2128104058d62a2
Simon MacDonald <40661454@live.napier.ac.uk>
2024-10-10 15:40:50 +0100

    Added basic Javadoc comments to App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,12 @@
+//Main
 package com.napier.sem;
 
 public class App {
 
+    /**
+     * Test string to check if dockerfiles work
+     * @param args
+     */
     public static void main(String[] args)
     {
         System.out.println("Test String");
