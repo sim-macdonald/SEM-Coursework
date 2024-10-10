e661ddd1e45a15ea411fda3b18951d6807cb08fa
Simon MacDonald <40661454@live.napier.ac.uk>
2024-10-10 14:33:17 +0100

    Added basic Comments to App

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,9 @@
+//Main method for the app
 package com.napier.sem;
 
 public class App {
 
+    //Test string to see if it runs.
     public static void main(String[] args)
     {
         System.out.println("Test String");
